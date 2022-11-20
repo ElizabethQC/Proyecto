6223451ed406e143060a1969d344f816bc702bb9
Elizabeth Quiroga Castro <becquiroga@gmail.com>
2022-11-20 13:38:21 -0300

    formularios de registro de pacientes y doctores

--- Clinica/Clinica/ClinicaApp/forms.py
+++ Clinica/Clinica/ClinicaApp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from models import Pacientes, Doctores
 
 class PacientesFormulario(forms.Form):
     nombre = forms.CharField(max_length=20)
@@ -16,3 +17,31 @@ class EspecialidadesFormulario(forms.Form):
 class ConsultoriosFormulario(forms.Form):
     nro_consultorio = forms.IntegerField()
     direccion = forms.CharField(max_length=20)
+
+class PacienteRegisterForm(UserCreationForm):
+    nombre =forms.CharField(max_length=20)
+    apellido = forms.CharField(max_length=20)
+    dni = forms.IntegerField()
+    telefono = forms.IntegerField()
+    email = forms.EmailField()
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repetir contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Pacientes
+        fields = ["nombre", "apellido", "dni", "telefono", "email", "password1", "password2"]
+        help_texts = {k:"" for k in fields}
+
+class DoctorRegisterForm(UserCreationForm):
+    nombre =forms.CharField(max_length=20)
+    apellido = forms.CharField(max_length=20)
+    dni = forms.IntegerField()
+    telefono = forms.IntegerField()
+    email = forms.EmailField()
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repetir contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Doctores
+        fields = ["nombre", "apellido", "dni", "telefono", "email", "password1", "password2"]
+        help_texts = {k:"" for k in fields}
--- Clinica/Clinica/ClinicaApp/views.py
+++ Clinica/Clinica/ClinicaApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 from .models import ObrasSociales, Especialidades, Pacientes, Consultorios, Horarios
 from .forms import OSFormulario, EspecialidadesFormulario
@@ -148,3 +150,49 @@ class HorarioDelete(DeleteView):
     model = Horarios
     template_name = "horarios/horario__delete.html"
     success_url = "/clinica-app/"    
+
+def register(request):
+
+      if request.method == 'POST':
+
+            #form = UserCreationForm(request.POST)
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+
+                  username = form.cleaned_data['username']
+                  form.save()
+                  return render(request,"AppCoder/inicio.html" ,  {"mensaje":"Usuario Creado :)"})
+
+      else:
+            #form = UserCreationForm()       
+            form = UserRegisterForm()     
+
+      return render(request,"AppCoder/registro.html" ,  {"form":form})
+
+
+def login_request(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+
+        if form.is_valid():  # Si pasó la validación de Django
+
+            usuario = form.cleaned_data.get('username')
+            contrasenia = form.cleaned_data.get('password')
+
+            user = authenticate(username= usuario, password=contrasenia)
+
+            if user is not None:
+                login(request, user)
+
+                return render(request, "AppCoder/inicio.html", {"mensaje":f"Bienvenido {usuario}"})
+            else:
+                return render(request, "AppCoder/inicio.html", {"mensaje":"Datos incorrectos"})
+           
+        else:
+
+            return render(request, "AppCoder/inicio.html", {"mensaje":"Formulario erroneo"})
+
+    form = AuthenticationForm()
+
+    return render(request, "AppCoder/login.html", {"form": form})
